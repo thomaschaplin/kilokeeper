33e153db095d7e4dbe64efa15898ae3b85b4f296
Thomas Chaplin <thomaschaplin@outlook.com>
2025-03-18 17:21:03 +0000

    updates

--- main.go
+++ main.go
@@ -4,14 +4,25 @@ import (
 	"encoding/json"
 	"fmt"
 	"log"
+	"math"
 	"net/http"
 	"os"
+	"strconv"
 	"time"
 )
 
 type WeightEntry struct {
 	Date      string  `json:"date"`
 	Kilograms float64 `json:"kilograms"`
+}
+
+type WeightResponse struct {
+	Date      string  `json:"date"`
+	Kilograms float64 `json:"kilograms"`
+	Stones    string  `json:"stones"`
+	Age       int     `json:"age"`
+	Bmi       string  `json:"bmi"`
+	BmiStatus string  `json:"bmiStatus"`
 }
 
 var weights []WeightEntry
@@ -36,9 +47,101 @@ func saveJSON(filename string, data interface{}) error {
 	return encoder.Encode(data)
 }
 
+func convertKilogramsToStonesPoundsOunces(weightInKilograms float64) string {
+	// Convert kilograms to pounds
+	weightInPounds := weightInKilograms * 2.20462
+
+	// Calculate the number of stones
+	stones := int(math.Floor(weightInPounds / 14))
+
+	// Calculate the remaining pounds
+	remainingPounds := int(math.Floor(math.Mod(weightInPounds, 14)))
+
+	// Calculate the remaining ounces
+	ounces := int(math.Round(math.Mod(weightInPounds, 1) * 16))
+
+	return fmt.Sprintf("%d st %d lb %d oz", stones, remainingPounds, ounces)
+}
+
+func calculateAge(dob string, currentDate string) (int, error) {
+	// Parse dates in DD/MM/YYYY format
+	today, err := time.Parse("02/01/2006", currentDate)
+	if err != nil {
+		return 0, fmt.Errorf("invalid current date format: %v", err)
+	}
+
+	birthDate, err := time.Parse("02/01/2006", dob)
+	if err != nil {
+		return 0, fmt.Errorf("invalid date of birth format: %v", err)
+	}
+
+	age := today.Year() - birthDate.Year()
+	if today.Month() < birthDate.Month() || (today.Month() == birthDate.Month() && today.Day() < birthDate.Day()) {
+		age--
+	}
+
+	return age, nil
+}
+
+func calculateBMI(weightInKilograms, height float64) float64 {
+	return weightInKilograms / math.Pow(height, 2)
+}
+
+func calculateBmiStatus(bmiValue float64) string {
+	if bmiValue < 18.5 {
+		return "Underweight"
+	} else if bmiValue <= 24.9 {
+		return "Normal"
+	} else if bmiValue <= 29.9 {
+		return "Overweight"
+	} else if bmiValue <= 39.9 {
+		return "Obese"
+	} else if bmiValue >= 40 {
+		return "Morbidly Obese"
+	}
+	return "Invalid data"
+}
+
 func getWeightsHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(weights)
+
+	dob := os.Getenv("DOB")
+	if dob == "" {
+		http.Error(w, "Date of birth (DOB) environment variable not set", http.StatusInternalServerError)
+		return
+	}
+
+	height := os.Getenv("HEIGHT")
+	if height == "" {
+		http.Error(w, "Height (HEIGHT) environment variable not set", http.StatusInternalServerError)
+		return
+	}
+
+	var response []WeightResponse
+	for _, entry := range weights {
+
+		age, err := calculateAge(dob, entry.Date)
+		if err != nil {
+			http.Error(w, "Error calculating age", http.StatusInternalServerError)
+			return
+		}
+
+		heightFloat, err := strconv.ParseFloat(height, 64)
+		if err != nil {
+			http.Error(w, "Invalid height format", http.StatusInternalServerError)
+			return
+		}
+
+		response = append(response, WeightResponse{
+			Date:      entry.Date,
+			Kilograms: entry.Kilograms,
+			Stones:    convertKilogramsToStonesPoundsOunces(entry.Kilograms),
+			Age:       age,
+			Bmi:       fmt.Sprintf("%.1f", calculateBMI(entry.Kilograms, heightFloat)),
+			BmiStatus: calculateBmiStatus(calculateBMI(entry.Kilograms, heightFloat)),
+		})
+	}
+	json.NewEncoder(w).Encode(response)
 }
 
 func addWeightHandler(w http.ResponseWriter, r *http.Request) {
